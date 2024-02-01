39374a9acb48883fd904231633a663f7ae3ddcf4
Jayabharatha Reddy Sadanagiri <Jayabharathreddys@gmail.com>
2024-02-01 23:05:35 +0530

    Next changes

--- src/main/java/com/jb/welcomeapi/rest/RestController.java
+++ src/main/java/com/jb/welcomeapi/rest/RestController.java
@@ -1,11 +1,16 @@
-package com.jb.welcomeapi;
+package com.jb.welcomeapi.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
+
+    @Autowired
+    Environment env;
     @GetMapping("/welcome")
     public String getWelcomeMsg(){
-        return "Welcome to JB IT";
+        return "Welcome to JB IT (" +env.getProperty("server.port")+")";
     }
 }
